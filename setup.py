a6ed2425b7531457f31cd313682487794a88ff15
Alfonso Irarrázaval <auirarrazaval@uc.cl>
2023-05-11 19:12:53 -0400

    Release 1.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="tether_utils",
     install_requires=["boto3", "drf-jwt", "djangorestframework", "drf-yasg", "iso3166"],
-    version="1.2.3",
+    version="1.3.1",
     author="Alfonso Irarrázaval",
     author_email="alfonso@tether.education",
     description=("Thin wrapper for Tether Utils / AWS SNS and Others"),
@@ -29,3 +29,4 @@ setuptools.setup(
 # 1.2.1 -> added iso3166 country utils
 # 1.2.2 -> typings and update author
 # 1.2.3 -> fix on imports
+# 1.3.0 -> added country serializers

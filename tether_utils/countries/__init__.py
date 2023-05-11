b1d83b5f53d2a088abb4f99fd8aa03096a919a83
Alfonso Irarrázaval <auirarrazaval@uc.cl>
2023-05-11 17:06:30 -0400

    fix: import validation

--- tether_utils/countries/__init__.py
+++ tether_utils/countries/__init__.py
@@ -1,10 +1,10 @@
 from .constants import COUNTRIES_TUPLES
-from .utils import get_country_code, validate_country_code
+from .utils import get_country_code, validate_country_string
 from .exceptions import CountryValidationException
 
 __all__ = [
     "get_country_code",
-    "validate_country_code",
+    "validate_country_string",
     "COUNTRIES_TUPLES",
     "CountryValidationException",
 ]

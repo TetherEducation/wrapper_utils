709695a1d93fc3df7c92c943319415509e1557c6
Alfonso Irarrázaval <31079536+auirarrazaval@users.noreply.github.com>
2023-05-11 16:52:01 -0400

    chore: typing and author (#4)

--- tether_utils/countries/constants.py
+++ tether_utils/countries/constants.py
@@ -1,3 +1,6 @@
 from iso3166 import countries
+from typing import Final, Tuple, Iterator
 
-COUNTRIES_TUPLES = ((c.alpha3, c.name) for c in countries)
+COUNTRIES_TUPLES: Final[Iterator[Tuple[str, str]]] = (
+    (c.alpha3, c.name) for c in countries
+)

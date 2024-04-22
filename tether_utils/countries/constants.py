bde12372e51a6b6292981bf1e301feeab88793ef
Alfonso Irarrázaval <auirarrazaval@uc.cl>
2024-04-22 14:35:32 +0200

    [CHORE] Enhanced countries

--- tether_utils/countries/constants.py
+++ tether_utils/countries/constants.py
@@ -1,8 +1,20 @@
-from iso3166 import countries
+import pycountry
 from typing import Final, Tuple, Iterator
 
-__all__ = ["COUNTRIES_TUPLES"]
+__all__ = ["COUNTRIES_TUPLES", "COUNTRIES_ISO2_TUPLES", "COUNTRIES_ISO3_TUPLES", "pycountry"]
 
-COUNTRIES_TUPLES: Final[Iterator[Tuple[str, str]]] = (
-    (c.alpha3, c.name) for c in countries
+COUNTRIES_ISO3_TUPLES: Final[Iterator[Tuple[str, str]]] = (
+    (c.alpha_3, c.name) for c in pycountry.countries
 )
+"""
+A tuple of tuples containing the ISO 3166-1 alpha-3 code and the country name.
+"""
+
+COUNTRIES_TUPLES = COUNTRIES_ISO3_TUPLES
+
+COUNTRIES_ISO2_TUPLES: Final[Iterator[Tuple[str, str]]] = (
+    (c.alpha_2, c.name) for c in pycountry.countries
+)
+"""
+A tuple of tuples containing the ISO 3166-1 alpha-2 code and the country name.
+"""

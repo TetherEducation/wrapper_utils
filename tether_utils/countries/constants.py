b2368d58dbdd2bc16138f4ef0c69cf45abd77bf4
Alfonso Irarrazaval <alfonso@tether.education>
2024-10-21 18:58:05 +0200

    chore: tether utils generators to lists

--- tether_utils/countries/constants.py
+++ tether_utils/countries/constants.py
@@ -3,18 +3,18 @@ from tether_utils.countries.utils.lookup import all_countries
 
 __all__ = ["COUNTRIES_TUPLES", "COUNTRIES_ISO2_TUPLES", "COUNTRIES_ISO3_TUPLES"]
 
-COUNTRIES_ISO3_TUPLES: Final[Iterator[Tuple[str, str]]] = (
+COUNTRIES_ISO3_TUPLES: Final[Iterator[Tuple[str, str]]] = [
     (c.iso.alpha_3, c.name) for c in all_countries()
-)
+]
 """
 A tuple of tuples containing the ISO 3166-1 alpha-3 code and the country name.
 """
 
 COUNTRIES_TUPLES = COUNTRIES_ISO3_TUPLES
 
-COUNTRIES_ISO2_TUPLES: Final[Iterator[Tuple[str, str]]] = (
+COUNTRIES_ISO2_TUPLES: Final[Iterator[Tuple[str, str]]] = [
     (c.iso.alpha_2, c.name) for c in all_countries()
-)
+]
 """
 A tuple of tuples containing the ISO 3166-1 alpha-2 code and the country name.
 """
